e65d03deeeadcd7e2caf466d41de8ce2c0edfae2
Daddy2054 <stivali-tinte-0w@icloud.com>
2024-11-14 21:28:07 +0100

    211. Saving a New Actor

--- Movies/Screens/MovieListScreen.swift
+++ Movies/Screens/MovieListScreen.swift
@@ -11,13 +11,27 @@ import SwiftData
 
 struct MovieListScreen: View {
     
+    @Environment(\.modelContext) private var context
+    
     @Query(sort: \Movie.title, order: .forward) private var movies: [Movie]
     @State private var isAddMoviePresented: Bool = false
+    @State private var isActorPresented: Bool = false
+    @State private var actorName: String = ""
+    
+    private func saveActor()    {
+        let actor = Actor(name: actorName)
+        context.insert(actor)
+    }
     
     var body: some View {
         MovieListView(movies: movies)
             .toolbar(content: {
-            ToolbarItem(placement: .topBarTrailing) {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button("Add Actor") {
+                        isActorPresented = true
+                    }
+                }
+                ToolbarItem(placement: .topBarTrailing) {
                 Button("Add Movie") {
                     isAddMoviePresented = true
                 }
@@ -28,12 +42,27 @@ struct MovieListScreen: View {
                 AddMovieScreen()
             }
         })
+        .sheet(isPresented: $isActorPresented, content: {
+//            NavigationStack {
+//                AddMovieScreen()
+//            }
+            Text("Add Actor").font(.largeTitle)
+ 
+            TextField("Actor Name", text: $actorName)
+                .textFieldStyle(.roundedBorder)
+                .presentationDetents([.fraction(0.25)])
+                .padding()
+            Button("Save") {
+                isActorPresented = false
+                saveActor()
+            }
+        })
     }
 }
 
 #Preview {
     NavigationStack {
         MovieListScreen()
-            .modelContainer(for: [Movie.self])
+            .modelContainer(for: [Movie.self, Review.self, Actor.self])
     }
 }

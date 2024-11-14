b0ce1a5c08fbb4cef010fa14c1d943325a958b63
Daddy2054 <stivali-tinte-0w@icloud.com>
2024-11-14 22:33:03 +0100

    213. Selecting Actors When Adding a New Movie

--- Movies/Screens/AddMovieScreen.swift
+++ Movies/Screens/AddMovieScreen.swift
@@ -14,37 +14,47 @@ struct AddMovieScreen: View {
     
     @State private var title: String = ""
     @State private var year: Int?
+    @State private var selectedActors: Set<Actor> = []
     
     private var isFormValid: Bool {
-        !title.isEmptyOrWhitespace && year != nil
+        !title.isEmptyOrWhitespace && year != nil && !selectedActors.isEmpty
     }
     var body: some View {
         Form {
             TextField("Title", text: $title)
             TextField("Year", value: $year, format:  .number)
+            Section("Select Actors") {
+                ActorSelectionView(selectedActors: $selectedActors)
+            }
         }.navigationTitle("Add Movie")
-        .toolbar {
-            ToolbarItem(placement: .topBarLeading) {
-                Button("Close"){
-                    dismiss()
-                }
-            }
-            ToolbarItem (placement: .topBarTrailing){
-                Button("Save") {
-                    guard let year = year else { return }
-                    let movie = Movie(title: title, year: year)
-                    context.insert(movie)
-                    
-                    do {
-                        try context.save()
-                    } catch {
-                        print("Error saving: \(error)")
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button("Close"){
+                        dismiss()
                     }
-                    dismiss()
                 }
-                .disabled(!isFormValid)
+                ToolbarItem (placement: .topBarTrailing){
+                    Button("Save") {
+                        guard let year = year else { return }
+                        let movie = Movie(title: title, year: year)
+                        movie.actors = Array(selectedActors)
+                        
+                        selectedActors.forEach { actor in
+                            actor.movies.append(movie)
+                            context.insert(actor)
+                        }
+                        context.insert(movie)
+                        
+                        do {
+                            try context.save()
+                        } catch {
+                            print("Error saving: \(error)")
+                        }
+                        dismiss()
+                    }
+                    .disabled(!isFormValid)
+                }
             }
-        }
     }
 }
 
